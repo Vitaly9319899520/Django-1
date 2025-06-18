243bbf0e81feef8525a0b18975c7c890bd6b65be
Maslykou Vitalii <9319899520@mail.ru>
2025-06-18 21:55:06 +0300

    11 comm

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from catalog.models import Product,Category
 
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("id",'name','price', 'category')
