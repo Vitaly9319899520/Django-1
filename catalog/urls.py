aa42cca144cb0920fabd69c551670743bbe04e18
Maslykou Vitalii <9319899520@mail.ru>
2025-06-26 16:37:54 +0300

    12 comm

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import home, contacts
+from catalog.views import catalog_list, catalog_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts')
+    # path('', home, name='home'),
+    # path('contacts/', contacts, name='contacts'),
+    path('',catalog_list, name='catalog_list'),
+    path('catalog/<int:pk>/',catalog_detail, name='catalog_detail')
 ]
 
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
+
 
 def home(request):
     return render(request,'home.html')
 
 def contacts(request):
     return render(request,'contacts.html')
+
+def catalog_list(request):
+    catalog = Product.objects.all()
+    context = {'catalog': catalog}
+    return render(request,'catalog_list.html', context)
+
+def catalog_detail(request, pk):
+    product = get_object_or_404(Product,pk=pk)
+    context = {'Product': product}
+    return render(request, 'catalog_detail.html', context)
+
